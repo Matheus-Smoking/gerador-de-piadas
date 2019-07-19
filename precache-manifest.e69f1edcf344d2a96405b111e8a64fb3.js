b1391b643e8a88bb5117e4d89c423703d82caf6c
matheussmoking <matheus-smoking@hotmail.com>
2019-07-19 17:05:28 -0300

    Updates

---
+++ precache-manifest.e69f1edcf344d2a96405b111e8a64fb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c313e341574fcff7b4a5441251b13d1a",
+    "url": "/gerador-de-piadas/index.html"
+  },
+  {
+    "revision": "6cccaa76e7a66a74d791",
+    "url": "/gerador-de-piadas/static/js/2.ad591a15.chunk.js"
+  },
+  {
+    "revision": "d4c6fa25047b83d557c9",
+    "url": "/gerador-de-piadas/static/js/main.e4a878df.chunk.js"
+  },
+  {
+    "revision": "d163589e7b56b7fd70cc",
+    "url": "/gerador-de-piadas/static/js/runtime~main.552fa9b8.js"
+  },
+  {
+    "revision": "1dd8e68b19d063a2b2c7c11cf1541b80",
+    "url": "/gerador-de-piadas/static/media/logo-chuck.1dd8e68b.png"
+  }
+]);
