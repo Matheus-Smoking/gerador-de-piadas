a37a08e12f18607a88c8943c8155f3bbaf08487b
matheussmoking <matheus-smoking@hotmail.com>
2019-07-19 16:47:33 -0300

    build

---
+++ build/precache-manifest.57ee0b6bf599ac62302eaaaec7ed4266.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cb68534340043462410d0f0a3107020",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fec19cfc77bdf1b6024",
+    "url": "/static/js/2.c8b15257.chunk.js"
+  },
+  {
+    "revision": "3c876f0934398476e0ac",
+    "url": "/static/js/main.873517f3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1dd8e68b19d063a2b2c7c11cf1541b80",
+    "url": "/static/media/logo-chuck.1dd8e68b.png"
+  }
+]);
