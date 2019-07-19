5eae5c6bcbc929c812f32e1d5aa14108c869254e
matheussmoking <matheus-smoking@hotmail.com>
2019-07-18 21:48:29 -0300

    created structure and fetchs

--- src/app.js
+++ src/app.js
@@ -4,10 +4,47 @@ class App extends Component {
 
   constructor() {
     super();
+    this.state = {
+      categories: [],
+      joke:'',
+    }
+      
+    
   }
+
+  componentWillMount() {
+    fetch(`https://api.chucknorris.io/jokes/categories`)
+    .then(res => res.json())
+    .then(res => this.setState({ categories: res }))
+    
+  }
+
+  metodoClick(name){
+    console.log(name)
+    fetch(`https://api.chucknorris.io/jokes/random?category=${name}`)
+    .then(res => res.json())
+    .then(res => this.setState({ joke: res }))
+  }
+
   render() {
     return (
-      <h1>Helo word teste12356</h1>
+      <main>
+      <h1>Gerador de Piadas- Chuck Norris</h1>
+      <ul>
+        {
+          this.state.categories.map((item, key) => {
+            return(
+              <li key={key} onClick={() => this.metodoClick(item)}>{item}</li>
+            )
+          }
+          )}
+      </ul>
+        <section>
+            <p>{this.state.joke.value}</p>
+            <p onClick={() => this.metodoClick(this.state.joke.categories)} >Proxima</p>
+        </section>
+
+      </main>
     );
   }
 }
