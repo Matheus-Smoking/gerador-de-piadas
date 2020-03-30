31ea4837e8f99ce72ab29ec07f315dad9a1b53c1
Matheus Smoking <matheus-smoking@hotmail.com>
2020-03-29 23:20:42 -0300

    :fire: delete Redux

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-const Category = (state ='Nenhuma', action ) =>{
-    switch (action.type){
-      case 'ADDSTATE' : return state = action.lastCategory;
-    }
-    return state
-  }
-  
-  export default Category;
